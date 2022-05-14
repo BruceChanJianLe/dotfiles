fd3f601c5f868cb835a4e442c074b74d07a18226
Bruce Chan Jian Le <jianle001@e.ntu.edu.sg>
2022-05-15 00:19:50 +0800

    update init.lua and plugins.lua

---
+++ lua/user/plugins.lua
@@ -0,0 +1,19 @@
+-- Plugin manager from https://github.com/wbthomason/packer.nvim
+
+local fn = vim.fn
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if fn.empty(fn.glob(install_path)) > 0 then
+  packer_bootstrap = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+end
+
+return require('packer').startup(function(use)
+  -- My plugins here
+  use 'tpope/vim-surround'
+  use 'kyazdani42/nvim-tree.lua'
+
+  -- Automatically set up your configuration after cloning packer.nvim
+  -- Put this at the end after all plugins
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)

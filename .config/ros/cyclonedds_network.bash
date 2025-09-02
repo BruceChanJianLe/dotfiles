2935529e52e9696ba9cd4a9d60b0872cab251fe1
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-09-02 17:09:17 +0800

    wip: add bash script to set autoware cyclonedds network

---
+++ .config/ros/cyclonedds_network.bash
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Increase the maximum receive buffer size for network packets
+sudo sysctl -w net.core.rmem_max=2147483647  # 2 GiB, default is 208 KiB
+
+# IP fragmentation settings
+sudo sysctl -w net.ipv4.ipfrag_time=3  # in seconds, default is 30 s
+sudo sysctl -w net.ipv4.ipfrag_high_thresh=134217728  # 128 MiB, default is 256 KiB

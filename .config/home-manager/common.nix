46744e7128b34437a428f67aefee7606c60fbd4a
Chan Jian Le <jianle001@e.ntu.edu.sg>
2026-08-09 13:20:59 +0800

    wip: ctrl+enter to start ghostty terminal

--- .config/home-manager/common.nix
+++ .config/home-manager/common.nix
@@ -8,4 +8,23 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
+
+  # Ensures Home Manager has permission to write to the GNOME dconf database
+  dconf.enable = true;
+
+  dconf.settings = {
+    # 1. Register your two custom keybinding profile paths
+    "org/gnome/settings-daemon/plugins/media-keys" = {
+      custom-keybindings = [
+        "/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/"
+      ];
+    };
+
+    # 2. Define the 'Ghostty' shortcut
+    "org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0" = {
+      name = "Ghostty";
+      command = "ghostty";
+      binding = "<Super>Return";
+    };
+  };
 }

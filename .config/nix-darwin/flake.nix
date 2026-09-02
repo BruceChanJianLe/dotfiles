c25a579fa4a1aa0e63c27720098b45fa51a5ba73
Chan Jian Le <jianle001@e.ntu.edu.sg>
2026-09-02 11:00:19 +0800

    fix: fat fingers

--- .config/nix-darwin/flake.nix
+++ .config/nix-darwin/flake.nix
@@ -66,7 +66,7 @@
         "developer" = mkHost { user = "developer"; };
         "chanjl" = mkHost { user = "chanjl"; };
         "raus" = mkHost { user = "raus"; };
-        "samuel" = mkHost { user = "sameul"; };
+        "samuel" = mkHost { user = "samuel"; };
       };
     };
 }
